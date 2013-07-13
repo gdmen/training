b64252bb4a41e4f6ff0cd4c284199c9722803638
unknown <gmenezes@seas.upenn.edu>
2013-07-12 20:26:00 -0700

    Replaces techniques page with list view.
    Also:
    - Adds tag creation.
    - Backs up database.

--- build_techniques.php
+++ build_techniques.php
@@ -1,6 +1,9 @@
 <?php
+// TODO: add tags to post body.
+
 $dir = 'technique_dictionary/';
-$node_type = 'technique_post';
+$content_type = 'technique_post';
+$taxonomy_machine_name = 'technique_tags';
 $uid = '1';
 
 function getNameFromFile($filename) {
@@ -16,6 +19,7 @@ function getTechniquesFromDir($dir) {
   }
   return $return;
 }
+
 
 $techniques = getTechniquesFromDir($dir);
 
@@ -35,17 +39,15 @@ function propogateReferences($techniques) {
     $changes = false;
     foreach($techniques as $name => $t_source) {
       foreach($t_source->getTo() as $other) {
-        // skip self references
-        if($t_source->getName() === $other ||
-           !isset($techniques[$other])) {
+        // skip self references and unchanged references
+        if($t_source->getName() === $other || !isset($techniques[$other])) {
           continue;
         }
         $changes |= $techniques[$other]->addFrom($t_source->getName());
       }
       foreach($t_source->getFrom() as $other) {
-        // skip self references
-        if($t_source->getName() === $other ||
-           !isset($techniques[$other])) {
+        // skip self references and unchanged references
+        if($t_source->getName() === $other || !isset($techniques[$other])) {
           continue;
         }
         $changes |= $techniques[$other]->addTo($t_source->getName());
@@ -54,23 +56,23 @@ function propogateReferences($techniques) {
   }
 }
 
-//print_r($techniques);
-
-
 define('DRUPAL_ROOT', getcwd());
 $_SERVER['REMOTE_ADDR'] = "localhost"; // Necessary if running from command line
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-$taxonomy = array();
-$result = db_query("SELECT tid, name FROM {taxonomy_term_data}");
-if($result) {
-  while($row = $result->fetchAssoc()) {
-    $taxonomy[$row['name']] = $row['tid'];
-  }
+function createTag($tag, $parent=0) {
+  $term = array(
+    'vid' => 2,
+    'name' => $tag,
+    'parent' => $parent,
+  );
+  $term = (object) $term;
+  taxonomy_term_save($term);
+  return $term->tid;
 }
 
-function update($log_name, &$old, $new, &$log) {
+function updateField($log_name, &$old, $new, &$log) {
   if ($old === $new) {
     return false;
   }
@@ -81,7 +83,7 @@ function update($log_name, &$old, $new, &$log) {
 foreach($techniques as $t) {
   $node = NULL;
   $node_exists = db_query("SELECT nid FROM {node} WHERE UPPER(title) = UPPER(:title) AND type = :type",
-    array(':title' => $t->getName(), ':type' => $node_type)
+    array(':title' => $t->getName(), ':type' => $content_type)
   );
   if($node_exists) {
     $row = $node_exists->fetchAssoc();
@@ -94,18 +96,18 @@ foreach($techniques as $t) {
     continue;
   }
   
-  $mt = new MarkdownTechnique($t, $taxonomy);
+  $mt = new MarkdownTechnique($t);
   
   $changed = False;
   $log = '';
-  $changed |= update('Title', $node->title, $mt->getTitle(), $log);
-  $changed |= update('Tags', $node->field_technique_tags[$node->language], $mt->getTags(), $log);
-  $changed |= update('Type', $node->type, $node_type, $log);
-  $changed |= update('Language', $node->language, LANGUAGE_NONE, $log);
-  $changed |= update('uid', $node->uid, $uid, $log);
-  $changed |= update('Body', $node->body[$node->language][0]['value'], $mt->getBody(), $log);
-  $changed |= update('Summary', $node->body[$node->language][0]['summary'], $mt->getSummary(), $log);
-  $changed |= update('Format', $node->body[$node->language][0]['format'], 'markdown', $log);
+  $changed |= updateField('Title', $node->title, $mt->getTitle(), $log);
+  $changed |= updateField('Tags', $node->field_technique_tags[$node->language], $mt->getTags(), $log);
+  $changed |= updateField('Type', $node->type, $content_type, $log);
+  $changed |= updateField('Language', $node->language, LANGUAGE_NONE, $log);
+  $changed |= updateField('uid', $node->uid, $uid, $log);
+  $changed |= updateField('Body', $node->body[$node->language][0]['value'], $mt->getBody(), $log);
+  $changed |= updateField('Summary', $node->body[$node->language][0]['summary'], $mt->getSummary(), $log);
+  $changed |= updateField('Format', $node->body[$node->language][0]['format'], 'markdown', $log);
 
   // Only write node if it was changed.
   if($changed) {
@@ -124,8 +126,10 @@ class Technique {
   private $raw = '';
   // Name.
   private $name = '';
-  // TechniqueType
-  private $type;
+  // Taxonomy Category
+  private $category = '';
+  // Taxonomy Subcategory
+  private $subcategory = '';
   // List of taxonomy terms.
   private $tags = [];
   // List of technique names from which to attempt this technique.
@@ -146,8 +150,6 @@ class Technique {
     $this->raw = $raw;
     
     $this->parseTo();
-    $this->parseTags();
-    $this->parseType();
     $this->parseFrom();
   }
   public function getName() {
@@ -162,8 +164,11 @@ class Technique {
   public function getTags() {
     return $this->tags;
   }
-  public function getType() {
-    return $this->type;
+  public function getCategory() {
+    return $this->category;
+  }
+  public function getSubcategory() {
+    return $this->subcategory;
   }
   public function getNotes() {
     return $this->notes;
@@ -176,6 +181,7 @@ class Technique {
   }
   
   public function completeParse() {
+    $this->parseCategories();
     $this->parseNotes();
     $this->parseDirections();
     $this->parseEscapes();
@@ -240,26 +246,16 @@ class Technique {
       $this->from = array_map("trim", $this->from);
     }
   }
-  private function parseTags() {
-    preg_match_all("/^Type:(?P<select>[^\n]*)/i", $this->raw, $m);
-    $this->tags = preg_split("/\s*,\s*/", $m['select'][0]);
-    $this->tags = array_map("trim", $this->tags);
-    $replace = function($str) {
-      return str_replace(' ', '-', $str);
-    };
-    $this->tags = array_map($replace, $this->tags);
-  }
-  private function parseType() {
-    if(!$this->tags) {
-      $this->parseTags();
+  private function parseCategories() {
+    preg_match_all("/Category:(?P<select>[^\n]*)/i", $this->raw, $m);
+    if($m['select']) {
+      $this->category = trim($m['select'][0]);
     }
-    foreach($this->tags as $tag) {
-      if($type = TechniqueType::reverseLookup($tag)) {
-        $this->type = $type;
-        return true;
-      }
+    $m = array();
+    preg_match_all("/Subcategory:(?P<select>[^\n]*)/i", $this->raw, $m);
+    if($m['select']) {
+      $this->subcategory = trim($m['select'][0]);
     }
-    return false;
   }
 }
 
@@ -270,15 +266,18 @@ class MarkdownTechnique {
   private $notes = '';
   private $directions = '';
   private $escapes = '';
+  // Taxonomy (locally fetched)
+  private $taxonomy = [];
   // input: technique object
-  public function __construct($technique, $taxonomy) {
-    $technique->completeParse();
-    $this->title = $this->generateTitle($technique);
-    $this->tags = $this->generateTags($technique, $taxonomy);
-    $this->header = $this->generateHeader($technique);
-    $this->notes = $this->generateNotes($technique);
-    $this->directions = $this->generateDirections($technique);
-    $this->escapes = $this->generateEscapes($technique);
+  public function __construct($t) {
+    $t->completeParse();
+    $this->taxonomy = $this->fetchTaxonomy();
+    $this->title = $this->generateTitle($t);
+    $this->tags = $this->generateTags($t);
+    $this->header = $this->generateHeader($t);
+    $this->notes = $this->generateNotes($t);
+    $this->directions = $this->generateDirections($t);
+    $this->escapes = $this->generateEscapes($t);
   }
   public function getTitle() {
     return $this->title;
@@ -292,28 +291,46 @@ class MarkdownTechnique {
   public function getBody() {
     return $this->header . "\n\n" . $this->notes . "\n\n" . $this->directions . "\n\n" . $this->escapes;
   }
-  private function generateTitle($technique) {
-    return ucwords(strtolower($technique->getName()));
-  }
-  private function generateTags($technique, $taxonomy) {
-    //TODO: Right now it ignores tags that don't already exist
-    $new_tags = array();
-    foreach($technique->getTags() as $tag) {
-      if(isset($taxonomy[$tag])) {
-        $new_tags[] = ['tid' => $taxonomy[$tag]];
+  private function fetchTaxonomy() {
+    $ret = array();
+    $result = db_query("SELECT T.tid, T.name FROM {taxonomy_term_data} as T, {taxonomy_vocabulary} as V WHERE T.vid = V.vid AND V.machine_name = :machine_name", array(':machine_name' => 'technique_tags'));
+    if($result) {
+      while($row = $result->fetchAssoc()) {
+        $ret[$row['name']] = $row['tid'];
       }
     }
-    return $new_tags;
+    return $ret;
   }
-  private function generateHeader($technique) {
-    $h = ["Type: " . join(', ', array_map(function($i) {
-      return "[[path:".$i."|".$i."]]"; }, $technique->getTags()))];
-    $h[] = "";
+  private function generateTitle($t) {
+    return ucwords(strtolower($t->getName()));
+  }
+  private function generateTags($t) {
+    $generated = array();
+    if($t->getCategory() != '') {
+      if(!isset($this->taxonomy[$t->getCategory()])) {
+        print("Adding top level category: " . $t->getCategory() . "\n");
+        $this->taxonomy[$t->getCategory()] = createTag($t->getCategory());
+      }
+      $generated[] = ['tid' => $this->taxonomy[$t->getCategory()]];
+      if($t->getSubcategory() != '') {
+        if(!isset($this->taxonomy[$t->getSubcategory()])) {
+          print("Adding subcategory: " . $t->getSubcategory() . "\n");
+          $this->taxonomy[$t->getSubcategory()] = createTag($t->getSubcategory(), $this->taxonomy[$t->getCategory()]);
+        }
+        $generated[] = ['tid' => $this->taxonomy[$t->getSubcategory()]];
+      }
+    }
+    return $generated;
+  }
+  private function generateHeader($t) {
+    //$h = ["Type: " . join(', ', array_map(function($i) {
+    //  return "[[path:".$i."|".$i."]]"; }, $t->getTags()))];
+    $h = [];
     $h[] = "Transition From: " . join(', ', array_map(function($i) {
-      return "[[".$i."|".$i."]]"; }, $technique->getFrom()));
+      return "[[".$i."|".$i."]]"; }, $t->getFrom()));
     $h[] = "";
     $h[] = "Transition To: " . join(', ', array_map(function($i) {
-      return "[[".$i."|".$i."]]"; }, $technique->getTo()));
+      return "[[".$i."|".$i."]]"; }, $t->getTo()));
     return join("\n", $h);
   }
   private function replaceLinks($str) {
@@ -329,42 +346,29 @@ class MarkdownTechnique {
     ];
     return preg_replace($patterns, $replacements, $str);
   }
-  private function generateNotes($technique) {
+  private function generateNotes($t) {
     $h = ["#### Notes:"];
     $h[] = "";
-    foreach($technique->getNotes() as $item) {
+    foreach($t->getNotes() as $item) {
       $text = $this->replaceLinks($item->text);
       $h[] = ($item->inset ? '' : '- ') . str_repeat('  ', $item->inset) . $text;
     }
     return join("\n\n", $h);
   }
-  private function generateDirections($technique) {
+  private function generateDirections($t) {
     $h = ["#### Directions:"];
     $h[] = "";
-    foreach($technique->getDirections() as $item) {
+    foreach($t->getDirections() as $item) {
       $text = $this->replaceLinks($item->text);
       $h[] = ($item->inset ? '' : '1. ') . str_repeat('  ', $item->inset) . $text;
     }
     return join("\n\n", $h);
   }
-  private function generateEscapes($technique) {
+  private function generateEscapes($t) {
     return '';
   }
 }
 
-class TechniqueType {
-  const POSITION = 'position';
-  const SUBMISSION = 'submission';
-  static function reverseLookup($str) {
-    switch(strtolower($str)) {
-      case TechniqueType::POSITION:
-        return TechniqueType::POSITION;
-      case TechniqueType::SUBMISSION:
-        return TechniqueType::SUBMISSION;
-    }
-    return false;
-  }
-}
 class ListItem {
   // Text.
   public $text = '';
